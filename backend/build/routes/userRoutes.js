3fe2f5bb3b905096271d14e538dc992ff656dde0
ZychGrzegorz <grzegorzzych95@gmail.com>
2021-02-24 02:30:23 +0100

    test

--- backend/build/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile, deleteUser, getUserById, updateUser } from '../controllers/userController.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
-var router = express.Router();
-router.route('/').post(registerUser).get(protect, admin, getUsers);
-router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser);
-export default router;
